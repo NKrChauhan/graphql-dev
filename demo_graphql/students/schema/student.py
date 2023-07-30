5a5facad241772599cc7f004e2cf6a3a683a2ad2
Nitish Kumar <nitish@pushowl.com>
2023-07-30 23:11:12 +0530

    feat(students): update mutation

--- demo_graphql/students/schema/student.py
+++ demo_graphql/students/schema/student.py
@@ -8,6 +8,46 @@ class StudentObjectType(DjangoObjectType):
         model = Student
         fields = ("name", "marks", "date_of_admission", "grade")
 
+
+class StudentCreationMutationModel(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        grade = graphene.String(required=True)
+        marks = graphene.Int(required=False)
+
+    student = graphene.Field(StudentObjectType)
+
+    def mutate(self, info, name, grade, **kwargs):
+        student = Student.objects.create(name=name, grade=grade, **kwargs)
+        return StudentCreationMutationModel(student=student)
+
+class StudentUpdateMutationModel(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        name = graphene.String()
+        marks = graphene.Int()
+        grade = graphene.String()
+
+    student = graphene.Field(StudentObjectType)
+
+    def mutate(self, info, id, **kwargs):
+        try:
+            student = Student.objects.get(pk=id)
+        except Student.DoesNotExist:
+            raise Exception("Student with the provided ID does not exist.")
+
+        for key, value in kwargs.items():
+            setattr(student, key, value)
+
+        student.save()
+        return StudentUpdateMutationModel(student=student)
+    
+
+class Mutation(graphene.ObjectType):
+    create_student_entry = StudentCreationMutationModel.Field()
+    update_student_entry = StudentUpdateMutationModel.Field()
+
+
 class Query(graphene.ObjectType):
     all_students = graphene.List(StudentObjectType, grade=graphene.String(required=False), limit=graphene.Int(required=False))
 
@@ -18,21 +58,5 @@ class Query(graphene.ObjectType):
             return Student.objects.filter(**api_filter).order_by('-date_of_admission')[:limit]
         return Student.objects.filter(**api_filter)
       
-
-class StudentMutationModel(graphene.Mutation):
-    class Arguments:
-        name = graphene.String(required=True)
-        grade = graphene.String(required=True)
-        marks = graphene.Int(required=False)
-
-    student = graphene.Field(StudentObjectType)
-
-    def mutate(self, info, name, grade, **kwargs):
-        student = Student.objects.create(name=name, grade=grade, **kwargs)
-        return StudentMutationModel(student=student)
-
-class Mutation(graphene.ObjectType):
-    create_student_entry = StudentMutationModel.Field()
-
 
 student_schema = graphene.Schema(query=Query, mutation=Mutation)
